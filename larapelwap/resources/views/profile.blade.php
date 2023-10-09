bb80eb96a05132b4c8fcdeca094949fc7cd3ef4e
Adityaalikusuma <>
2023-10-09 13:13:54 +0700

    karina 2226250058

---
+++ larapelwap/resources/views/profile.blade.php
@@ -0,0 +1,1 @@
+<h1> Welcome to Rock Bottom </h1>
--- larapelwap/routes/web.php
+++ larapelwap/routes/web.php
@@ -16,3 +16,45 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// buat route halaman profil
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+// route dengan parameter
+
+// Route::get('/ mahasiswa/{nama?}', function ($nama = 'Karina') {
+//     echo "<h2>hlow smwanya</h2>";
+//     echo "Nama saya $nama";
+// });
+ // route dengan parameter >1
+
+ Route::get('/mahasiswa/{nama?}/{pekerjaan?}', function ($nama = 'Karina', $pekerjaan
+  = 'mahasiswa'){
+    echo "<h2>Hloww smwanyh</h2>";
+    echo "Nama saya $nama, sebagai $pekerjaan";
+  });
+
+  //redirect
+
+  Route::get("/hubungi", function(){
+    echo "<h1Contact Us</h1>";
+  })->name('/call');
+
+  Route::redirect("/contact", "/hubungi");
+
+  Route::get("/halo", function(){
+    echo "<a href=", route('call'), "'>", "</a>";
+  });
+
+  Route::prefix('/dosen')->group(function(){
+    Route::get('/jadwal', function() {
+        echo "<h2>jadwal</h2>";
+    });
+
+    Route::get('/jadwal', function() {
+        echo "<h2>materi perkuliahan</h2>";
+    });
+    });
