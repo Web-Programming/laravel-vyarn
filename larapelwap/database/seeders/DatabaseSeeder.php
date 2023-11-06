a6c49160c1ae644862774c834a0f36661068fa73
vyarn <110971257+vyarn@users.noreply.github.com>
2023-11-06 13:16:29 +0700

    eloquent-Relationship
    eloquent 2

--- larapelwap/database/seeders/DatabaseSeeder.php
+++ larapelwap/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Prodi;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -10,13 +12,24 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Prodi::create(
+       [
+            'nama' => 'Teknik Informatika'
+       ]
+    );
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+    Prodi::create(
+        [
+            'nama' => 'Manajemen Informatika'
+        ]
+        );
+
+    Prodi::create (
+        [
+            'nama' => 'Sistem Informasi'
+        ]
+    );
+   }
 }
